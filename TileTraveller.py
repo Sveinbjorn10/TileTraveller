8931f1ef1fb1a53f846394b4cba37226509d0af6
Sveinbjorn10 <Sveinbjorn10@hotmail.com>
2018-09-18 14:41:18 +0000

    Fyrsti helmingur búinn og byrjaður á seinni

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,65 @@
-print("Sveinki Hakkari")
+user = 11
+
+
+T = "You can travel: "
+N = "(N)orth"
+E = "(E)ast"
+S = "(S)outh"
+W = "(W)est"
+OR = " or "
+P = "."
+
+Tile11 = T + N + P
+Tile12 = T + N + OR + E + OR + S + P
+Tile13 = T + E + OR + S + P
+Tile21 = T + N + P
+Tile22 = T + S + OR + W + P
+Tile23 = T + E + OR + W + P
+Tile31 = T
+Tile32 = T + N + OR + S + P
+Tile33 = T + S + OR + W + P
+
+def options(tilenr):
+    print(tilenr)
+
+def movement(tilenr):
+    while True:
+        user_input = input("Direction: ").capitalize()
+        if user_input in tilenr:
+            if user_input == "N":
+                return 1
+            elif user_input == "E":
+                return 10
+            elif user_input == "S":
+                return -1
+            else:
+                return -10
+        else:
+            print("Not a valid direction!")
+
+tilenum = Tile11 
+
+while True:
+    if user == 11:
+        tilenum = Tile11
+    elif user == 12:
+        tilenum = Tile12
+    elif user == 13:
+        tilenum = Tile13
+    elif user == 21:
+        tilenum = Tile21
+    elif user == 22:
+        tilenum = Tile22
+    elif user == 23:
+        tilenum = Tile23
+    elif user == 32:
+        tilenum = Tile32
+    elif user == 33:
+        tilenum = Tile33
+    else:
+        break
+
+    options(tilenum)
+    user += movement(tilenum)
+
+print("Victory!")    
---
+++ TileTravellerIdiotVersion.py
@@ -0,0 +1,141 @@
+# Í þessum kóða nota ég while lykkju sem er alltaf sönn. 
+# Ég mun brjóta hana þegar ég er kominn með userinn í stöðu 3.1.
+# Inn í þeirri lykkju er ég með if setningar fyrir hvern og einn glugga í völundarhúsinu.
+# Þar á eftir prenta ég út þær hreyfingar sem notandi má taka í hverjum og einum glugga.
+# Þar inn í set ég inn while lykkju sem biður um áttina sem aðilinn vill fara í.
+# í þessum lykkjum athuga ég hvort leyfileg átt hefur verið valin. Ef ekki þá kemur villumelding
+# og lykkjan fer annan hring og biður um nýja átt. Ef rétt átt er valin þá hækkar núverandi staða
+# í samræmi við  þá átt sem notandinn vill taka.
+# Svo loks þegar einstaklingur hefur komist í reit 3.1 þá brýt ég aðal while lykkjuna og tilkynni
+# notanda að hann hafi unnið!
+
+user = 1.1
+
+North = "nN"
+East = "eE"
+South = "sS"
+West = "wW"
+
+user_input = ""
+
+while True:
+    if user == 1.1:
+        print("You can travel: (N)orth.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in North:
+                user += 0.1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 1.2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in North:
+                user += 0.1
+                user = round(user, 2)
+                break
+            elif user_input in East:
+                user += 1
+                user = round(user, 2)
+                break
+            elif user_input in South:
+                user -= 0.1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 1.3:
+        print("You can travel: (E)ast or (S)outh.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in East:
+                user += 1
+                user = round(user, 2)
+                break
+            elif user_input in South:
+                user -= 0.1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 2.1:
+        print("You can travel: (N)orth.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in North:
+                user += 0.1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 2.2:
+        print("You can travel: (S)outh or (W)est.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in South:
+                user -= 0.1
+                user = round(user, 2)
+                break
+            elif user_input in West:
+                user -= 1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 2.3:
+        print("You can travel: (E)ast or (W)est.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in East:
+                user += 1
+                user = round(user, 2)
+                break
+            elif user_input in West:
+                user -= 1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 3.1:
+        break
+
+    if user == 3.2:
+        print("You can travel: (N)orth or (S)outh.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in North:
+                user += 0.1
+                user = round(user, 2)
+                break
+            elif user_input in South:
+                user -= 0.1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+    if user == 3.3:
+        print("You can travel: (S)outh or (W)est.")
+        while True:
+            user_input = input("Direction: ")
+            if user_input in South:
+                user -= 0.1
+                user = round(user, 2)
+                break
+            elif user_input in West:
+                user -= 1
+                user = round(user, 2)
+                break
+            else:
+                print("Not a valid direction!")
+
+print("Victory!")
